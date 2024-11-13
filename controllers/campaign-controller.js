62969dce2b8f0109ab54d6b32a02e27ced63fc7a
João Alves <jpcoutinhoalves@gmail.com>
2024-11-13 20:12:22 +0000

    feat: add campaign-controller with campaigns rendering

---
+++ controllers/campaign-controller.js
@@ -0,0 +1,31 @@
+/** 
+ * Render the campaigns.
+ *
+ * @param{import("express").Request} req - The express request object.
+ * @param{import("express").Response} res - The express response object.
+ *
+ * @returns{void}
+ */
+function viewCampaigns(req, res) {
+	// TODO: fetch user from user repository or session variable
+	let user = "admin"
+
+	// TODO: fetch campaign from campaigns repo
+	let campaigns = [
+		{
+			title: "Title",
+			creator: null,
+			description: "Description.",
+			goal: 100000,
+			donationsPercentage: 0,
+			updates: null,
+			creator: {
+				username: "name",
+			},
+		}
+	]
+
+	res.render("home", { user, campaigns });
+}
+
+module.exports = { viewCampaigns }
