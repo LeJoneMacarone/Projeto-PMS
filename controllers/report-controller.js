547201720bd4756a24c6d198f892fae46670c613
eusebioponte1000 <114869764+eusebioponte1000@users.noreply.github.com>
2024-11-20 20:21:38 +0000

    feat: report info page shows campaign info (title, creator) with dynamic data

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -4,7 +4,7 @@ const { Report, Campaign, User } = require('../db/sequelize').models;
 exports.getAllReports = async (req, res) => {
     try {
         const { user } = req.session;
-        if(user.role == "administrator"){
+        if (user.role == "administrator") {
             const reports = await Report.findAll({
                 include: [
                     { model: Campaign, as: 'campaign' },
@@ -12,9 +12,9 @@ exports.getAllReports = async (req, res) => {
                 ]
             });
 
-            res.render('admin_validate_reports_view', { user , reports: reports });
-            //res.status(200).json(reports);
-        }else{
+            res.render('admin_validate_reports_view', { user, reports: reports });
+            //res.status(200).json(reports); //show data for debugging
+        } else {
             res.redirect("/login");
         }
     } catch (error) {
@@ -26,24 +26,45 @@ exports.getAllReports = async (req, res) => {
 exports.getReportById = async (req, res) => {
     try {
         const { user } = req.session;
-        if(user.role == "administrator"){
-            const report = await Report.findByPk(req.params.id, {
-                include: [
-                    { model: Campaign, as: 'campaign' },
-                    { model: User, as: 'reporter' }
-                ]
-            });
-            
-            if (!report) {
-                return res.status(404).json({ error: 'Report not found' });
-            }
-
-            res.render('admin_validate_report_info', { user , report: report });
-            //res.status(200).json(report);
-        }else{
+        if (!user) {
             res.redirect("/login");
         }
-        
+        if (user.role !== "administrator") {
+            res.redirect("/login");
+        }
+
+        const report = await Report.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Campaign, as: 'campaign',
+                    include: [
+                        {
+                            model: User,
+                            as: 'creator',
+                        },
+                    ],
+                },
+                { model: User, as: 'reporter' }
+            ]
+        });
+
+        if (!report) {
+            return res.status(404).json({ error: 'Report not found' });
+        }
+
+        if(!report.campaign){
+            // TODO if report exists but campaign dont then it should delete the report (redirect to deletion)
+            return res.status(404).json({ error: 'Campaign not found' }); 
+        }
+
+        if(!report.campaign.creator){
+            // TODO if campaign exists but creator dont then it should delete every campaign of that creator id (redirect to deletion)
+            return res.status(404).json({ error: 'Campaign Creator not found' });
+        }
+
+        res.render('admin_validate_report_info', { user, report: report });
+        //res.status(200).json(report); //show data for debugging
+
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
