155943e06eeed0a600b415e7778d989838060e9b
João Alves <jpcoutinhoalves@gmail.com>
2024-11-26 19:16:59 +0000

    refactor: use in memory storage for multer

--- utils/upload.js
+++ utils/upload.js
@@ -1,9 +1,6 @@
 const multer = require("multer");
 
-// TODO: if needed, use this snippet to access the entire file's contents at req.file.buffer
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage });
-
-const upload = multer({ dest: "public/uploads/" });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 module.exports = upload;
