07c499d9a1a8360835278c1649650c28f02f9206
João Alves <jpcoutinhoalves@gmail.com>
2024-11-30 15:35:05 +0000

    fix: duplicated code on user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -39,8 +39,6 @@ function renderLoginPage(req, res) {
 function renderProfilePage(req, res) {
 	const { user, error } = req.session;
 	req.session.error = "";
-	const { user, error } = req.session;
-	req.session.error = "";
 
 	if (!user) {
 		req.session.error = "Log in to access the profile page.";
